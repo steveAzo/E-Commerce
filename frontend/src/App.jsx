f812e92ebd24e36040c17c1211c7d8a5870489c2
steveAzo -Git <stephenazongo77@gmail.com>
2024-02-12 21:51:36 +0000

    something

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar1 from "./components/Nav1.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <Navbar1 />
+        <h1>Hello World</h1>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
